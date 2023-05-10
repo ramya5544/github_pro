2e379fe3ac19864f81bef82632748e6d2edd5fc5
ramya5544 <ramyavishvhin1995@gmail.com>
2023-05-10 22:23:25 +0530

    css

---
+++ ubercalculation.js
@@ -0,0 +1,10 @@
+class Student{
+    constructor(){
+        this.name="ramya";
+        this.age="29";
+        this.location="salem";
+
+    }
+}
+let Student101=new Student();
+console.log(Student101);
